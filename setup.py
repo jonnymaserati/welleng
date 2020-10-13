82b9b3da4ad66f3e620c48d7f9c94620e139fd04
jonnymaserati <jonnycorcutt@gmail.com>
2020-10-13 14:37:51 +0200

    Make working module

---
+++ setup.py
@@ -0,0 +1,27 @@
+from setuptools import setup, find_packages
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setup(
+    name='welleng',
+    version='0.1.0',    
+    description='A collection of Well Engineering modules',
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url='https://github.com/jonnymaserati/welleng',
+    author='Jonathan Corcutt',
+    author_email='jonnycorcutt@gmail.com',
+    license='MIT License',
+    packages=find_packages(),
+    install_requires=['pandas',
+                      'numpy',                     
+                      ],
+
+    classifiers=[
+        'License :: OSI Approved :: MIT License',  
+        'Operating System :: OS Independent',        
+        'Programming Language :: Python :: 3',
+    ],
+    python_requires='>=3.8',
+)
---
+++ welleng/__init__.py
@@ -0,0 +1,4 @@
+import welleng.clearance
+import welleng.io
+import welleng.data
+import welleng.utils
