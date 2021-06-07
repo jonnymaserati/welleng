1361082132f43ef4181e91d23ba6aba17d042bb7
Jonny <41046859+jonnymaserati@users.noreply.github.com>
2021-06-07 14:00:41 +0200

    Update connector.py
    Revert back to previous version.

--- welleng/connector.py
+++ welleng/connector.py
@@ -829,7 +829,7 @@ class Connector:
             if self.error:
                 self.radius_critical = ((self.md_target - self.md1) / (
                     abs(self.dogleg) + abs(self.dogleg2)
-                )) / 2
+                ))
                 assert self.radius_critical > 0
                 # self.radius_critical += np.random.rand() * (
                 #     self.radius_critical - self.radius_critical2
