db4cacc0095c71fd3b464a8de54591dd29035293
jonnymaserati <jonnycorcutt@gmail.com>
2021-11-20 13:52:03 +0100

    Add  property to Survey

--- welleng/units.py
+++ welleng/units.py
@@ -16,14 +16,23 @@ units_default = {
     'diameters': 'meters',
     'lateral_distances': 'meters',
     'dls': "deg / _30m",
-    'turn_rates': 'deg / _30m'
+    'turn_rates': 'deg / _30m',
+    'magnetic_field': 'nT',
+    'angular_velocity': 'rad / hr',
+    'acceleration': 'meters / sec ** 2'
 }
 
 # map the parameters to the unit groups
 parameters = {
-    'angles': ['dogleg', 'toolface'],
-    'depths': ['md', 'tvd', 'z', 'curve_radius'],
+    'angles': [
+        'dogleg', 'toolface', 'dip', 'declination', 'convergence',
+        'vertical_inc_limit', 'vertical_section_azimuth'
+    ],
+    'depths': ['md', 'tvd', 'z', 'curve_radius', 'altitude'],
     'diameters': ['radius'],
-    'lateral_distances': ['n', 'e', 'x', 'y'],
-    'turn_rates': ['build', 'turn']
+    'lateral_distances': ['n', 'e', 'x', 'y', 'vertical_section'],
+    'turn_rates': ['build', 'turn'],
+    'magnetic_field': ['b_total'],
+    'angular_velocity': ['earth_rate'],
+    'acceleration': ['G']
 }
