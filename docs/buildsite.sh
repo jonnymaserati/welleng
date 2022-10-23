684671ff348140ccd0d358646ef6e9b43ba4d246
jonnymaserati <jonnycorcutt@gmail.com>
2022-10-23 17:27:20 +0200

    try getting sphinx built on gh

--- docs/buildsite.sh
+++ docs/buildsite.sh
@@ -13,9 +13,8 @@ export SOURCE_DATE_EPOCH=$(git log -1 --pretty=%ct)
  
 # Python Sphinx, configured with source/conf.py
 # See https://www.sphinx-doc.org/
-cd ../docs
-make clean
-make html
+make -C ../docs clean
+make -C ../docs html
 
 #######################
 # Update GitHub Pages #
