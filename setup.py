ab30cd81dc13003c2791476809da10375bda6fdc
jonnymaserati <jonnycorcutt@gmail.com>
2023-03-29 23:54:01 +0200

    Update setup.py

--- setup.py
+++ setup.py
@@ -109,7 +109,7 @@ setup(
         'Natural Language :: English',
         'Topic :: Scientific/Engineering',
     ],
-    python_requires='>=3.7.*',
+    python_requires='>=3.10',
     packages=find_packages(exclude=["tests"]),
     package_data={
         'welleng': [
