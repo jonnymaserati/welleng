1b4580a4c24b3b21389144df22fb449469e281bd
Jonny <41046859+jonnymaserati@users.noreply.github.com>
2023-12-17 20:59:36 +0100

    Update utils.py
    Update the docstring to render correctly.

--- welleng/utils.py
+++ welleng/utils.py
@@ -666,7 +666,7 @@ def annular_volume(od: float, id: float = None, length: float = None):
         The (unit) volume of the annulus or cylinder.
 
     Examples
-    -------
+    --------
     In the following example we calculate annular volume along a 1,000 meter
     section length of 9 5/8" casing inside 12 1/4" hole.
 
