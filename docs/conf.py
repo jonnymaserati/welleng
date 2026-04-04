9816ad1b0e26cf7c819f281dafdfc9cb1bc7b9ac
Jonathan Corcutt <jonnycorcutt@gmail.com>
2026-04-04 16:56:33 +0200

    Docs: enable version display in theme

--- docs/conf.py
+++ docs/conf.py
@@ -91,6 +91,9 @@ pygments_style = 'sphinx'
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#options-for-html-output
 
 html_theme = 'sphinx_rtd_theme'
+html_theme_options = {
+    'display_version': True,
+}
 html_static_path = ['_static']
 
 # options for rtd-theme
